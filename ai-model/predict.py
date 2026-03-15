c0b18d17613db52a4857e5a713ee33d11275bb58
vedantbhusari18 <vedantbhusari18@gmail.com>
2026-03-15 21:40:58 +0530

    Update predict.py

--- ai-model/predict.py
+++ ai-model/predict.py
@@ -1,1 +1,16 @@
+import random
 
+def predict_crop(soil, weather):
+    if soil == "loamy" and weather == "rainy":
+        return "Rice"
+    elif soil == "sandy":
+        return "Groundnut"
+    else:
+        return "Wheat"
+
+if __name__ == "__main__":
+    soil = input("Enter soil type: ")
+    weather = input("Enter weather: ")
+
+    crop = predict_crop(soil, weather)
+    print("Recommended Crop:", crop)
