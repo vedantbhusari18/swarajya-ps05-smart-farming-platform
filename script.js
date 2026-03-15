f51cf3942d7e56d1d07783b50702a9e1a9202685
vedantbhusari18 <vedantbhusari18@gmail.com>
2026-03-16 02:00:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("AgriVeista App Started");
+
+// splash transition
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+}, 2000);
+
+});
