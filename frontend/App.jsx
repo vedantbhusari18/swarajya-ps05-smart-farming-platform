30ad09332dd9b874a626d407a3bfa2f0e12fa31e
vedantbhusari18 <vedantbhusari18@gmail.com>
2026-03-15 21:38:29 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import SmartCropDoctor from "./SmartCropDoctor";
 
+function App() {
+  return (
+    <div>
+      <h1>Smart Farming AI Platform</h1>
+      <SmartCropDoctor />
+    </div>
+  );
+}
+
+export default App;
